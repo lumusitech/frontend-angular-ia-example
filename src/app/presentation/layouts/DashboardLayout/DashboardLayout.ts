56a4eed7bf86798111ae07277a34489159b392f2
Luciano Figueroa <lumusitech@gmail.com>
2025-11-25 18:39:49 -0300

    feat: add base dashboardLayout

---
+++ src/app/presentation/layouts/DashboardLayout/DashboardLayout.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  imports: [],
+  templateUrl: './DashboardLayout.html',
+  styleUrl: './DashboardLayout.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardLayout { }
