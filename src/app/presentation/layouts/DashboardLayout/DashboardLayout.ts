83a457cc0db3d007325e7e7f6b7db06d76d53c05
Luciano Figueroa <lumusitech@gmail.com>
2025-11-27 01:00:42 -0300

    feat: add sidemenu for navigation

--- src/app/presentation/layouts/DashboardLayout/DashboardLayout.ts
+++ src/app/presentation/layouts/DashboardLayout/DashboardLayout.ts
@@ -1,10 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarMenuItem } from '../../components/sidebarMenuItem/sidebarMenuItem';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarMenuItem],
   templateUrl: './dashboardLayout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardLayout {}
+export class DashboardLayout {
+  //? Only pass defined routes with data property
+  public routes = routes[0].children?.filter((route) => route.data);
+}
