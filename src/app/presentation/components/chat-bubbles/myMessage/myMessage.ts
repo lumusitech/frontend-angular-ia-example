6de691aca3062d746fe25d90e146eec3b166a125
Luciano Figueroa <lumusitech@gmail.com>
2025-11-27 20:09:53 -0300

    feat: add response chat bubble

--- src/app/presentation/components/chat-bubbles/myMessage/myMessage.ts
+++ src/app/presentation/components/chat-bubbles/myMessage/myMessage.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-my-message',
@@ -6,4 +6,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './myMessage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MyMessage { }
+export class MyMessage {
+  text = input.required<string>();
+}
--- src/app/presentation/pages/orthographyPage/orthographyPage.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ChatMessage } from '../../components/chat-bubbles/chatMessage/chatMessage';
+import { MyMessage } from '../../components/chat-bubbles/myMessage/myMessage';
 
 @Component({
   selector: 'app-orthography-page',
-  imports: [ChatMessage],
+  imports: [ChatMessage, MyMessage],
   templateUrl: './orthographyPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
