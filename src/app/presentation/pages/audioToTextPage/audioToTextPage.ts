c8ca3fdf229223f7e5caf4e6334392a8bc5cacc3
Luciano Figueroa <lumusitech@gmail.com>
2026-01-14 11:24:58 -0300

    feat: add audio to text base page

--- src/app/presentation/pages/audioToTextPage/audioToTextPage.ts
+++ src/app/presentation/pages/audioToTextPage/audioToTextPage.ts
@@ -1,9 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  TextMessageEvent,
+  TextMessageBoxFile,
+  TypingLoader,
+  MyMessage,
+  ChatMessage,
+} from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-audio-to-text-page',
-  imports: [],
+  imports: [TextMessageBoxFile, TypingLoader, MyMessage, ChatMessage],
   templateUrl: './audioToTextPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AudioToTextPage {}
+export default class AudioToTextPage {
+  messages = signal<Message[]>([
+    { text: 'Hello world', isGpt: false },
+    { text: 'Hello world!!!', isGpt: true },
+  ]);
+  isLoading = signal(false);
+  openAiService = inject(OpenAiService);
+
+  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
+    console.log({ prompt, file });
+  }
+}
