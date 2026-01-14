4f3125770af7af289164fc7a1d2034208851572e
Luciano Figueroa <lumusitech@gmail.com>
2026-01-14 13:28:09 -0300

    feat: add audio to text feature

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,4 +1,5 @@
+export * from './audio-to-text.response';
 export * from './message.interface';
-export * from './pros-cons.response';
 export * from './orthography.response';
+export * from './pros-cons.response';
 export * from './translate.response';
--- src/app/presentation/pages/audioToTextPage/audioToTextPage.ts
+++ src/app/presentation/pages/audioToTextPage/audioToTextPage.ts
@@ -6,6 +6,7 @@ import {
   MyMessage,
   ChatMessage,
 } from '@components/index';
+import { AudioToTextResponse } from '@interfaces/audio-to-text.response';
 import { Message } from '@interfaces/message.interface';
 import { OpenAiService } from 'app/presentation/services/openai.service';
 
@@ -16,14 +17,47 @@ import { OpenAiService } from 'app/presentation/services/openai.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AudioToTextPage {
-  messages = signal<Message[]>([
-    { text: 'Hello world', isGpt: false },
-    { text: 'Hello world!!!', isGpt: true },
-  ]);
+  messages = signal<Message[]>([]);
   isLoading = signal(false);
   openAiService = inject(OpenAiService);
 
-  handleMessageWithFile({ prompt, file }: TextMessageEvent) {
-    console.log({ prompt, file });
+  handleMessageWithFile({ file, prompt }: TextMessageEvent) {
+    console.log({ fileName: file.name });
+
+    const text = prompt || file.name || 'transcribe audio';
+    this.isLoading.set(true);
+
+    this.messages.update((messages) => [...messages, { text, isGpt: false }]);
+
+    this.openAiService.audioToText(file, text).subscribe({
+      next: (resp) => this.handleResponse(resp),
+    });
+  }
+
+  handleResponse(resp: AudioToTextResponse | null) {
+    this.isLoading.set(false);
+
+    if (!resp) {
+      return;
+    }
+
+    const text = `
+    ## Audio Transcription
+    __Duration:__ ${Math.round(resp.duration)} seconds
+    ## Text:
+    ${resp.text}
+    `;
+
+    this.messages.update((messages) => [...messages, { text, isGpt: true }]);
+
+    //? Details about each text segment transcription with its start and end time.
+    for (const segment of resp.segments) {
+      const segmentText = `
+        __From: ${Math.round(segment.start)} seconds to: ${Math.round(segment.end)} seconds.__
+        ${segment.text}
+      `;
+
+      this.messages.update((messages) => [...messages, { text: segmentText, isGpt: true }]);
+    }
   }
 }
