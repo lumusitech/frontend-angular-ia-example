9a890a3170c73b6b25460f2e0dcd15e89debbbdf
Luciano Figueroa <lumusitech@gmail.com>
2026-01-15 10:28:07 -0300

    feat: add base tunning page

--- src/app/presentation/pages/imageTunningPage/imageTunningPage.ts
+++ src/app/presentation/pages/imageTunningPage/imageTunningPage.ts
@@ -1,9 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChatMessage, MyMessage, TextMessageBox, TypingLoader } from '@components/index';
+import { Message } from '@interfaces/message.interface';
+import { OpenAiService } from 'app/presentation/services/openai.service';
 
 @Component({
   selector: 'app-image-tunning-page',
-  imports: [],
+  imports: [ChatMessage, MyMessage, TextMessageBox, TypingLoader],
   templateUrl: './imageTunningPage.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ImageTunningPage {}
+export default class ImageTunningPage {
+  messages = signal<Message[]>([]);
+  isLoading = signal(false);
+  openAiService = inject(OpenAiService);
+
+  handleMessage(prompt: string) {
+    this.isLoading.set(true);
+    this.messages.update((prev) => [...prev, { text: prompt, isGpt: false }]);
+
+    this.openAiService.imageGeneration(prompt).subscribe((resp) => {
+      if (!resp) return;
+
+      this.messages.update((prev) => [...prev, { text: resp.alt, isGpt: true, imageInfo: resp }]);
+
+      this.isLoading.set(false);
+    });
+  }
+}
